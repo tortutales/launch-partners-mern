8de006f7635e04e6a413f214e914431e1fef3c88
jorius <josedrc97@gmail.com>
2020-04-08 21:48:44 -0500

    Change collection name property in user model

--- backend/src/models/user/index.js
+++ backend/src/models/user/index.js
@@ -36,7 +36,7 @@ const UsersSchema = new Schema({
         type: String,
         unique: true
     }
-}, { collection: 'user' });
+});
 
 /**
  * Delete password from user schema, to avoid returns it to user.
@@ -48,4 +48,4 @@ UsersSchema.set('toJSON', {
     }
 });
 
-module.exports = mongoose.model('User', UsersSchema);
+module.exports = mongoose.model('User', UsersSchema, 'users');
