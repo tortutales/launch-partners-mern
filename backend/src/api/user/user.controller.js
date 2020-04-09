f866d46da265fe19b744372da357d507743a1a32
jorius <josedrc97@gmail.com>
2020-04-08 23:54:57 -0500

    Add method to update user by id

--- backend/src/api/user/user.controller.js
+++ backend/src/api/user/user.controller.js
@@ -3,6 +3,9 @@ const { httpCodes } = require('../../constants');
 const { responseSchema } = require('../../utils/res');
 const { userService } = require('../../services');
 
+// @models
+const { User } = require('../../models');
+
 async function getAllUsersController(_req, res) {
     try {
         const users = await userService.getAll();
@@ -43,7 +46,33 @@ async function loginController(req, res) {
     }
 }
 
+async function updateUserController(req, res) {
+    const user = await User.findOne({ _id: req.body.id });
+
+    user.description = req.body.description;
+    user.name = req.body.name;
+
+    if (user.save()) {
+        responseSchema({
+            data: { user },
+            message: 'User updated succesfully',
+            res
+        });
+        return;
+    }
+
+    responseSchema({
+        data: { user },
+        error: true,
+        httpStatusCode: httpCodes.NOT_CONTENT,
+        message: 'There was an error updating the user',
+        res,
+        success: false
+    });
+}
+
 module.exports = {
     getAllUsersController,
-    loginController
+    loginController,
+    updateUserController
 };
