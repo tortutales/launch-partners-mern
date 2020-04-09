d32e05f9067774e8caa79ac8421611e9133ac31f
jorius <josedrc97@gmail.com>
2020-04-08 21:47:10 -0500

    Allow to debug mongoose connection

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,13 +1,32 @@
 // @packages
 const mongoose = require('mongoose');
 
+if (process.env.DEBUG) {
+    mongoose.set('debug', true);
+}
+
 // @scripts
 const { databaseConnection } = require('../config');
+const { globals } = require('../utils');
 
 // @constants
 const DATABASE_URL = `mongodb://${databaseConnection.host}:${databaseConnection.port}/${databaseConnection.database}`;
 
-// execute database connection
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+// Execute database connection.
+mongoose.connect(DATABASE_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Handle possible connection errors.
+mongoose.connection.on('error', () => {
+    globals.log('Error in MongoDB connection');
+});
+
+// Check once we're connected to MongoDB.
+mongoose.connection.once('open', () => {
+    globals.log('Connected to MongoDB');
+});
 
 module.exports = mongoose;
