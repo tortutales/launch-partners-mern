22124b162dd56fd1233bec114c7f96545bfc1125
jorius <josedrc97@gmail.com>
2020-04-08 22:39:29 -0500

    Migrate database once database connection is done

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -8,6 +8,7 @@ if (process.env.DEBUG) {
 // @scripts
 const { databaseConnection } = require('../config');
 const { globals } = require('../utils');
+const { migrateDatabase } = require('./migration');
 
 // @constants
 const DATABASE_URL = `mongodb://${databaseConnection.host}:${databaseConnection.port}/${databaseConnection.database}`;
@@ -27,6 +28,8 @@ mongoose.connection.on('error', () => {
 // Check once we're connected to MongoDB.
 mongoose.connection.once('open', () => {
     globals.log('Connected to MongoDB');
+    const { db: databaseContext } = mongoose.connection;
+    migrateDatabase(databaseContext);
 });
 
 module.exports = mongoose;
